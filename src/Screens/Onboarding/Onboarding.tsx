8e651530ebca1482f48aaedab2f0256fbcd682c2
Hung Pham <hung.pham@hcmut.edu.vn>
2023-12-16 04:20:55 +0700

    Complete feature

---
+++ src/Screens/Onboarding/Onboarding.tsx
@@ -0,0 +1,192 @@
+import React, { ClassAttributes, useEffect, useRef, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Animated,
+  ViewToken,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { RootScreens } from "..";
+import slides, { OnboardingItemType } from "./slides";
+import { OnboardingItem } from "./components/OnboardingItem";
+import { Paginator } from "./components/Paginator";
+import { ArrowForwardIcon } from "native-base";
+import { WelcomeItem } from "./components/WelcomItem";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const Onboarding = (props: {
+  onNavigate: (string: RootScreens) => void;
+}) => {
+  const { width } = useWindowDimensions();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slidesRef = useRef<FlatList>(null);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const viewableItemsChanged = useRef(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      setCurrentIndex(viewableItems[0].index as number);
+    }
+  ).current;
+
+  const isLastItem = () => currentIndex === slides.length - 1;
+
+  const isFirstItem = () => currentIndex === 0;
+
+  const scrollTo = async () => {
+    if (!isLastItem()) {
+      slidesRef.current?.scrollToIndex({ index: currentIndex + 1 });
+    } else {
+      props.onNavigate(RootScreens.MAIN);
+    }
+  };
+
+  useEffect(() => {
+    const setViewedOnboarding = async () => {
+      try {
+        await AsyncStorage.setItem("@viewedOnboarding", "true");
+      } catch (error) {
+        console.log("Error @setItem: ", error);
+      }
+    };
+    setViewedOnboarding();
+  }, []);
+
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) =>
+            item.id !== 0 ? (
+              <OnboardingItem item={item} />
+            ) : (
+              <WelcomeItem item={item} />
+            )
+          }
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          bounces={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            {
+              useNativeDriver: false,
+            }
+          )}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <View style={[styles.buttonNextContainer, { width }]}>
+          {isFirstItem() ? (
+            <View style={{ height: 10 }}></View>
+          ) : (
+            <Paginator
+              data={slides.slice(1) as OnboardingItemType[]}
+              currentIndex={currentIndex}
+            />
+          )}
+          <TouchableOpacity
+            style={[styles.buttonNext, isFirstItem() && styles.buttonInWelcome]}
+            onPress={scrollTo}
+          >
+            <Text
+              style={[
+                styles.buttonNextText,
+                isFirstItem() && styles.buttonInWelComeText,
+              ]}
+            >
+              {currentIndex === slides.length - 1
+                ? "Bắt đầu với Flavorly thôi"
+                : "Tiếp theo"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {!isLastItem() && (
+          <TouchableOpacity
+            style={styles.buttonStart}
+            onPress={() => props.onNavigate(RootScreens.MAIN)}
+          >
+            <View style={styles.buttonStartTextWrapper}>
+              <Text
+                style={[
+                  styles.buttonStartText,
+                  isFirstItem() && styles.buttonStartInWelcomeText,
+                ]}
+              >
+                Hoặc bắt đầu với Flavorly ngay!
+              </Text>
+            </View>
+            <ArrowForwardIcon size="18px" color="white" />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonsContainer: {
+    position: "absolute",
+    bottom: 0,
+    height: 120,
+  },
+  buttonNextContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 4,
+    rowGap: 10,
+    paddingHorizontal: 24,
+  },
+  buttonNextText: {
+    fontFamily: "Bold",
+    fontSize: 19,
+    textAlign: "center",
+  },
+  buttonStartText: {
+    color: "white",
+    fontFamily: "Bold",
+    fontSize: 13,
+  },
+  buttonStart: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 4,
+  },
+  buttonStartTextWrapper: {
+    width: 205,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonNext: {
+    backgroundColor: "white",
+    height: 54,
+    borderRadius: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  buttonInWelComeText: {
+    color: "white",
+  },
+  buttonInWelcome: {
+    backgroundColor: "#ECB390",
+  },
+  buttonStartInWelcomeText: {
+    color: "#32324D",
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as Localization from "expo-localization";
 import { i18n, Language } from "@/Localization";
 import { NativeBaseProvider } from "native-base";
@@ -6,12 +6,41 @@ import { store, persistor } from "@/Store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { ApplicationNavigator } from "./Navigation";
+import {
+  Raleway_400Regular,
+  Raleway_700Bold,
+  useFonts,
+} from "@expo-google-fonts/raleway";
+
+import { CormorantSC_400Regular } from "@expo-google-fonts/cormorant-sc";
+
+import * as SplashScreen from "expo-splash-screen";
 
 i18n.locale = Localization.locale;
 i18n.enableFallback = true;
 i18n.defaultLocale = Language.ENGLISH;
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Regular: Raleway_400Regular,
+    Bold: Raleway_700Bold,
+    CormorantSC_400Regular,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return (
     <NativeBaseProvider>
       <Provider store={store}>
